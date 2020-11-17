d9fd273fb362e3bb40fa02c4b3c6043bcb3d815f
Ankitkj1999 <57802410+Ankitkj1999@users.noreply.github.com>
2020-11-17 23:21:57 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('GeeksforGeeks'),
+          backgroundColor: Colors.greenAccent[400],
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Menu',
+            onPressed: () {},
+          ), //IcoButton
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.comment),
+              tooltip: 'Comment',
+              onPressed: () {},
+            ), //IconButton
+          ], //<Widget>[]
+        ), //AppBar
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: SizedBox(
+              height: 250,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: NetworkImage(
+                        'https://media.geeksforgeeks.org/wp-content/cdn-uploads/logo.png'),
+                  ),
+                  border: Border.all(
+                      color: const Color(0xFF000000),
+                      width: 4.0,
+                      style: BorderStyle.solid), //Border.all
+                ), //BoxDirection
+              ),
+            ),
+          ),
+        ), //Center
+      ), //Scaffold
+      debugShowCheckedModeBanner: false,
+    ), //MaterialApp
+  );
+}
